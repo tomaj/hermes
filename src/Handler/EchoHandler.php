64d03f0fd5d253e12360981a663518baffbfe823
Tomas Majer <tomasmajer@gmail.com>
2015-10-22 10:31:10 +0200

    Renamed dump handler to echo handler

---
+++ src/Handler/EchoHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tomaj\Hermes\Handler;
+
+use Tomaj\Hermes\MessageInterface;
+
+class EchoHandler implements HandlerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(MessageInterface $message)
+    {
+        echo "Received message: #{$message->getId()} (type {$message->getType()})\n";
+        $payload = json_encode($message->getPayload());
+        echo "Payload: {$payload}\n";
+        return true;
+    }
+}
