d2721523a6f9d24771c212257b5d59513429d810
Tomas Majer <tomasmajer@gmail.com>
2015-10-22 10:31:22 +0200

    Added examples

---
+++ examples/redis/emitter.php
@@ -0,0 +1,21 @@
+<?php
+
+use Tomaj\Hermes\Driver\RedisSetDriver;
+use Tomaj\Hermes\Dispatcher;
+use Tomaj\Hermes\Message;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+$driver = new RedisSetDriver($redis);
+
+$dispatcher = new Dispatcher($driver);
+
+$counter = 1;
+while (true) {
+	$dispatcher->emit(new Message('type1', ['message' => $counter]));
+	echo "Emited message $counter\n";
+	$counter++;
+	sleep(1);
+}
---
+++ examples/redis/processor.php
@@ -0,0 +1,18 @@
+<?php
+
+use Tomaj\Hermes\Driver\RedisSetDriver;
+use Tomaj\Hermes\Dispatcher;
+use Tomaj\Hermes\Message;
+use Tomaj\Hermes\Handler\EchoHandler;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+$driver = new RedisSetDriver($redis);
+
+$dispatcher = new Dispatcher($driver);
+
+$dispatcher->registerHandler('type1', new EchoHandler());
+
+$dispatcher->handle();
