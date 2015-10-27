83327c2a4c53f2f71e4a393d6483480cbba796db
Tomas Majer <tomasmajer@gmail.com>
2015-10-27 17:23:48 +0100

    General credentials in examples

--- examples/rabbitmq/emitter.php
+++ examples/rabbitmq/emitter.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 
 $queueName = 'hermes_queue';
-$connection = new AMQPStreamConnection('localhost', 5672, 'stanka', '123', '/stanka');
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest', '/');
 $channel = $connection->channel();
 $channel->queue_declare($queueName, false, false, false, false);
 $driver = new RabbitMqDriver($channel, $queueName);
