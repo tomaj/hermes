3c64119d9e316a8ca29484a9fede81785716fa8b
Tomas Majer <tomasmajer@gmail.com>
2015-10-27 21:28:29 +0100

    Added test for echo handler

---
+++ tests/drivers/EchoHandlerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tomaj\Hermes;
+
+use PHPUnit_Framework_TestCase;
+use Tomaj\Hermes\Handler\EchoHandler;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+class EchoHandlerTest extends PHPUnit_Framework_TestCase
+{
+    public function testEchoHandler()
+    {
+    	$message = new Message('message1key', ['a' => 'b']);
+    	$output = "Received message: #{$message->getId()} (type message1key)\n";
+    	$output .= "Payload: {\"a\":\"b\"}\n";
+    	$this->expectOutputString($output);
+		
+        $echoHandler = new EchoHandler();
+        $echoHandler->handle($message);
+    }
+
+}
