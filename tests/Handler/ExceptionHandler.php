31759b54cc8b1f2e91b2622e20ee6906a2d900b4
Tomas Majer <tomasmajer@gmail.com>
2016-02-28 10:07:49 +0100

    Initialized debugger in tests

--- tests/Handler/ExceptionHandler.php
+++ tests/Handler/ExceptionHandler.php
@@ -5,11 +5,13 @@ namespace Tomaj\Hermes\Test\Handler;
 use RuntimeException;
 use Tomaj\Hermes\MessageInterface;
 use Tomaj\Hermes\Handler\HandlerInterface;
+use Tracy\Debugger;
 
 class ExceptionHandler implements HandlerInterface
 {
     public function handle(MessageInterface $message)
     {
+    	Debugger::enable(Debugger::DETECT, __DIR__);
         throw new RuntimeException('Error in handler');
     }
 }
