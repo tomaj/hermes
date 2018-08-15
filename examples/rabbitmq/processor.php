5c150125d1a79c91112da13a0bcc68a58316de5c
Tomas Majer <tomasmajer@gmail.com>
2018-08-15 09:28:00 +0200

    Enabled strict_types

--- examples/rabbitmq/processor.php
+++ examples/rabbitmq/processor.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use Tomaj\Hermes\Driver\RabbitMqDriver;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
--- examples/redis/emitter.php
+++ examples/redis/emitter.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use Tomaj\Hermes\Driver\RedisSetDriver;
 use Tomaj\Hermes\Emitter;
